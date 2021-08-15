2208704a87d7b14f4f25eeff88625c859d15e7be
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-08-14 23:59:40 -0700

    Working on Contact Form Styling with React-Bootstrap.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,21 +1,60 @@
 import React from 'react';
-import { FaGithub, FaLinkedinIn, FaEnvelope } from 'react-icons/fa';
+import { FaEnvelope, FaMapMarker, FaPhone } from 'react-icons/fa';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Placeholder Text</h1>
-      <p>(206) 660-6505<br/>cmbellwold@gmail.com<br/></p>
+    <div className='container'>
+      <section className='contact'>
+        <h2 className='text-center'>Contact Me</h2>
+        <p className='text-center mx-auto'>I would love to hear from you!</p>
 
-        <a className='cmb-fa px-4' href='https://www.linkedin.com/in/cailinbellwold/' target='_blank' rel='noreferrer noopener'>
-          <FaLinkedinIn />
-        </a>
-        <a className='cmb-fa px-3' href='https://github.com/CailinBellWold' target='_blank' rel='noreferrer noopener'> 
-        <FaGithub />
-        </a>
-        <a className='cmb-fa px-3' href='mailto:cmbellwold@gmail.com?subject=Contact from Portfolio Site' target='_blank' rel='noreferrer noopener'>
-        <FaEnvelope />
-        </a>
+        <div className='col-md-9'>
+          <Form id='contact-form'>
+            <div className='row'>
+              <Form.Group className='col-md-6 mt-3'>
+                <Form.Control type='text' placeholder='Your name' aria-label='Your name'/>
+              </Form.Group>
+              <Form.Group className='col-md-6 mt-3'>
+                <Form.Control type='email' placeholder='Your email' aria-label='Your email'/>
+              </Form.Group>
+            </div>
+
+            <div className='row'>
+              <Form.Group className='col-md-12 mt-3'>
+                <Form.Control type='text' placeholder='Subject' aria-label='Subject'/>
+              </Form.Group>
+            </div>
+
+            <div className='row'>
+              <Form.Group className='col-md-12 mt-3'>
+                <Form.Control as='textarea' rows={5} placeholder='Your message' aria-label='Your message'/>
+              </Form.Group>
+            </div>
+
+            <Button variant='primary' type='submit'>Send</Button>
+            
+          </Form>
+        </div>
+
+        <div className='col-md-3 text-center'>
+          <ul className='list-unstyled mb-0'>
+            <li><FaMapMarker />
+              <p>Seattle, WA 98102, USA</p>
+            </li>
+
+            <li><FaPhone />
+              <p>+ 01 206 660 6505</p>
+            </li>
+
+            <li><FaEnvelope />
+              <p>cmbellwold@gmail.com</p>
+            </li>
+          </ul>
+        </div>
+
+      </section>
     </div>
   );
 }
