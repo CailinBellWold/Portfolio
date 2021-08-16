932224b3cf51e60345b87617392eda4b92653df4
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-08-15 22:10:21 -0700

    Working on integrating React-Form-Hooks into Contact Page.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,39 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaEnvelope, FaMapMarker, FaPhone } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 export default function Contact() {
-  function validateForm() {
-    var name =  document.getElementById('name').value;
-    if (name == "") {
-        document.querySelector('.status').innerHTML = "Name cannot be empty";
-        return false;
-    }
-    var email =  document.getElementById('email').value;
-    if (email == "") {
-        document.querySelector('.status').innerHTML = "Email cannot be empty";
-        return false;
-    } else {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(!re.test(email)){
-            document.querySelector('.status').innerHTML = "Email format invalid";
-            return false;
-        }
-    }
-    var subject =  document.getElementById('subject').value;
-    if (subject == "") {
-        document.querySelector('.status').innerHTML = "Subject cannot be empty";
-        return false;
-    }
-    var message =  document.getElementById('message').value;
-    if (message == "") {
-        document.querySelector('.status').innerHTML = "Message cannot be empty";
-        return false;
-    }
-    document.querySelector('.status').innerHTML = "Sending...";
-  };
-  
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [result, setResult] = useState('');
+    const onSubmit = (data) => setResult(JSON.stringify(data));
+
   return (
     <div className='container'>
       <section className='contact mb-5'>
@@ -42,53 +18,65 @@ export default function Contact() {
 
         <div className='row'>
           <div className='col-md-9'>
-            <Form id='contact-form'>
+            <Form id='contact-form' onSubmit={handleSubmit(onSubmit)}>
 
-              <div className='row'>
-                <Form.Group className='col-md-6 mt-3'>
-                  <Form.Control type='text' id='name' placeholder='Your name' aria-label='Your name'/>
-                </Form.Group>
-                <Form.Group className='col-md-6 mt-3'>
-                  <Form.Control type='email' id='email' placeholder='Your email' aria-label='Your email'/>
-                </Form.Group>
-              </div>
+            <div className='row'>
+              <Form.Group className='col-md-6 mt-3'>
+                <FloatingLabel controlId='floatingInput' label='Name'>
+                  <Form.Control {...register('name', { required: true, maxLength: 33 })} placeholder='Name' />
+                  {errors.name?.type === 'required' && `*Required`}
+                </FloatingLabel>
+              </Form.Group>
+              <Form.Group className='col-md-6 mt-3'>
+                <FloatingLabel controlId='floatingInput' label='Email'>
+                  <Form.Control {...register('email')} placeholder='Email' />
+                </FloatingLabel>
+              </Form.Group>
+            </div>
 
-              <div className='row'>
-                <Form.Group className='col-md-12 mt-3'>
-                  <Form.Control type='text' id='subject' placeholder='Message subject' aria-label='Message subject'/>
-                </Form.Group>
-              </div>
+            <div className='row'>
+              <Form.Group className='col-md-12 mt-3'>
+                <FloatingLabel controlId='floatingInput' label='Subject'>
+                  <Form.Control {...register('subject', { required: true, maxLength: 33 })} placeholder='Subject' />
+                  {errors.subject?.type === 'required' && `*Required`}
+                </FloatingLabel>
+              </Form.Group>
+            </div>
 
-              <div className='row'>
-                <Form.Group className='col-md-12 mt-3'>
-                  <Form.Control as='textarea' rows={5} id='message' placeholder='Your message' aria-label='Your message'/>
-                </Form.Group>
-              </div>
+            <div className='row'>
+              <Form.Group className='col-md-12 mt-3'>
+                <FloatingLabel controlId='floatingInput' label='Message'>
+                  <Form.Control as='textarea' style={{ height: '100px' }} {...register('message', { required: true, maxLength: 263 })}  placeholder='Message' />
+                  {errors.message?.type === 'required' && `*Required`}
+                </FloatingLabel>
+              </Form.Group>
+            </div>
 
-              <Button variant='primary' type='submit' onclick='validateForm()'>Send</Button>
-
+                  {/* <p>{result}</p> */}
+              <Button variant='primary' type='submit'>Send</Button>
             </Form>
           </div>
 
-          <div className='col-md-3 mt-2 text-center'>
-            <ul className='list-unstyled mb-0'>
-              <li className='mb-3'><FaMapMarker />
-                <p>Seattle, WA 98102, USA</p>
-              </li>
+<div className='col-md-3 mt-2 text-center'>
+  <ul className='list-unstyled mb-0'>
+    <li className='mb-3'><FaMapMarker />
+      <p>Seattle, WA 98102, USA</p>
+    </li>
 
-              <li className='my-3'><FaPhone />
-                <p>+ 01 206 660 6505</p>
-              </li>
+    <li className='my-3'><FaPhone />
+      <p>+ 01 206 660 6505</p>
+    </li>
 
-              <li className='my-3'><FaEnvelope />
-                <p>cmbellwold@gmail.com</p>
-              </li>
-            </ul>
-          </div>
+    <li className='my-3'><FaEnvelope />
+      <p>cmbellwold@gmail.com</p>
+    </li>
+  </ul>
+</div>
 
-        </div>
+</div>
 
-      </section>
-    </div>
+</section>
+</div>
+
   );
 }
